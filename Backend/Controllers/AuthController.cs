529b8093832febc2a2407b7fefe5fbcdef91ceb7
Alexandre Tessier <alexandre.tessier@hotmail.fr>
2025-06-27 21:19:48 -0400

    Ajout des controlleur de base, de l'objet User et mise a jour du schema

---
+++ Backend/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost(Name = "Login")]
+        public IActionResult Login()
+        {
+            return Ok("User Login successfully.");
+        }
+
+        [HttpPost(Name = "Logout")]
+        public IActionResult Logout()
+        {
+            return Ok("User Logout successfully.");
+        }
+    }
+}
---
+++ Backend/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost(Name = "Register")]
+        public IActionResult Register()
+        {
+            return Ok("User registered successfully.");
+        }
+
+        [HttpPost(Name = "Update")]
+        public IActionResult Update()
+        {
+            return Ok("User updated successfully.");
+        }
+    }
+}
