c455de3b6fb5d576fa1debd79d92c6f293098029
Alexandre Tessier <alexandre.tessier@hotmail.fr>
2025-08-05 13:24:54 -0400

    Add Token Validation

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,7 +1,10 @@
 using Backend.Data;
 using Backend.Interfaces;
 using Backend.Services;
-using Microsoft.EntityFrameworkCore; // Ajout de l'espace de noms requis pour AddDbContext  
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text; // Ajout de l'espace de noms requis pour AddDbContext  
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), sqlOptions =>
            sqlOptions.EnableRetryOnFailure())); ;
+
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var secret = jwtSettings["Secret"] ?? throw new Exception("Jwt:Secret is missing in appsettings");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret))
+    };
+});
 
 builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddTransient<IAuthService, AuthService>();
@@ -41,6 +66,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
