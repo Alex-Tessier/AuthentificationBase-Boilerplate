fa89cbad1c6212fac18598c137bbe65f2600e680
Alexandre Tessier <alexandre.tessier@hotmail.fr>
2025-08-05 10:26:39 -0400

    Update Home page

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,9 @@
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-4">Bienvenue sur AccessHub</h1>
-      <p className="text-lg text-gray-700 mb-8">Votre point d'accès sécurisé pour tous vos projets .NET & Re vact.</p>
-      <a href="/login" className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition">Se connecter</a>
+      <h1 className="text-4xl font-bold mb-4">Welcome on AuthentificationBase</h1>
+      <p className="text-lg text-gray-700 mb-8">Your secure access point for all your .NET & React projects.</p>
+      <a href="/login" className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition">Login</a>
     </div>
   );
 };
